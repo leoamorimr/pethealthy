d10f3fb5486497b372af2a0534e43c350230e984
Leonardo Amorim Ramos <leoamorimr@gmail.com>
2021-03-26 15:27:35 -0300

    Teste de branch

--- src/main/java/br/com/leoamorimr/pethealthy/PethealthyApplication.java
+++ src/main/java/br/com/leoamorimr/pethealthy/PethealthyApplication.java
@@ -8,6 +8,7 @@ public class PethealthyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PethealthyApplication.class, args);
+        System.out.println("Teste");
     }
 
 }
